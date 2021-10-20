6273a2b6146b808464f646b305de872b66cb00da
gabiliz <gabrielalizmoreira@gmail.com>
2021-10-20 20:29:03 -0300

    rota

--- mary_fifi/lib/routes.dart
+++ mary_fifi/lib/routes.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'package:mary_fifi/views/login.view.dart';
 import 'package:mary_fifi/views/create_room.view.dart';
+import 'package:mary_fifi/views/create_question.view.dart';
 
 const String loginPage = 'login.view';
 const String createRoom = 'create_room.view';
+const String createQuestion = 'create_question.view';
 
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
@@ -12,6 +14,8 @@ Route<dynamic> controller(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => LoginView());
     case createRoom:
       return MaterialPageRoute(builder: (context) => CreateRoom());
+    case createQuestion:
+      return MaterialPageRoute(builder: (context) => CreateQuestion());
     default:
       throw ('Erro!');
   }
