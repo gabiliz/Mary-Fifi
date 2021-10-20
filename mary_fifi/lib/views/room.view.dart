3984074459d7ffe69751fe8d687d7d3737262c8e
LuizKraisch <luizkraisch22@gmail.com>
2021-10-20 20:29:45 -0300

    foof

--- mary_fifi/lib/views/room.view.dart
+++ mary_fifi/lib/views/room.view.dart
@@ -4,10 +4,11 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mary_fifi/views/question.view.dart';
+import 'package:mary_fifi/views/room_footer.view.dart';
 import 'package:mary_fifi/views/room_header.view.dart';
 import '../src/constants.dart';
 import 'package:badges/badges.dart';
-import 'package:mary_fifi/routes.dart' as route;
+
 
 class Room extends StatelessWidget {
   @override
@@ -24,44 +25,13 @@ class Room extends StatelessWidget {
                   child: Column(
                     children: [
                       RoomHeader(),
-                      SizedBox(height: 10.0),
                       Question(),
                       SizedBox(height: 10.0),
                       Question(),
                       SizedBox(height: 10.0),
                       Question(),
                       SizedBox(height: 10.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          SvgPicture.asset(
-                              title,
-                              height: 50, width: 50,
-                              fit: BoxFit.scaleDown
-                          ),
-                          TextButton(
-                            onPressed: () =>
-                                Navigator.pushNamed(context, route.loginPage),
-                            style: ButtonStyle(
-                                padding: MaterialStateProperty.all<EdgeInsets>(
-                                    EdgeInsets.fromLTRB(50, 15, 50, 15)),
-                                backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.white),
-                                shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(
-                                            100.0)))),
-                            child: Text(
-                              "Sair da sala",
-                              style: GoogleFonts.lato(
-                                fontSize: 15.0,
-                                color: Color(0xff2A2235),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                      RoomFooter(),
                     ],
                   ),
                 ),
