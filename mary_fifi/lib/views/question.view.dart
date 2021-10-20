34642d2d6ed2fe943d9f6e467d5789b0e97b4c35
LuizKraisch <luizkraisch22@gmail.com>
2021-10-20 20:16:44 -0300

    Extraindo cabeçalho da página de sala para widget

--- mary_fifi/lib/views/question.view.dart
+++ mary_fifi/lib/views/question.view.dart
@@ -14,78 +14,81 @@ class _QuestionState extends State<Question> {
   Widget build(BuildContext context) {
     return Container(
       constraints: BoxConstraints(minWidth: 300),
-      child: Card(
-        color: secondaryColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(25.0),
-        ),
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Text(
-                'Olá, eu gostaria de saber como criar um componente funcional dentro do React e se existe diferença na forma de criar os componentes.',
-                style: GoogleFonts.lato(color: Colors.white, fontSize: 15.0),
-              ),
-              SizedBox(height: 5.0),
-              Row(children: <Widget>[
-                Chip(
-                  padding: EdgeInsets.all(0),
-                  backgroundColor: Colors.white,
-                  label: Text('React',
-                      style: GoogleFonts.lato(color: primaryColor, fontSize: 12.0)),
+      child: GestureDetector(
+        onTap: () => {},
+        child: Card(
+          color: secondaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Olá, eu gostaria de saber como criar um componente funcional dentro do React e se existe diferença na forma de criar os componentes.',
+                  style: GoogleFonts.lato(color: Colors.white, fontSize: 15.0),
                 ),
-                SizedBox(width: 10.0),
-                Chip(
-                  padding: EdgeInsets.all(0),
-                  backgroundColor: Colors.white,
-                  label: Text('Javascript',
-                      style: GoogleFonts.lato(color: primaryColor, fontSize: 12.0)),
-                ),
-              ]),
-              SizedBox(height: 10.0),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        SvgPicture.asset(logo,
-                            height: 40, width: 40, fit: BoxFit.scaleDown),
-                        SizedBox(width: 8.0),
-                        Text(
-                          'Rachel',
-                          style: GoogleFonts.lato(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    SizedBox(width: 15.0),
-                    Row(
-                      children: <Widget>[
-                        Row(children: <Widget>[
-                          IconButton(
-                              icon: FaIcon(
-                                  FontAwesomeIcons.solidCheckCircle,
-                                  color: Colors.white),
-                              onPressed: () {
-                                print("Pressed");
-                              }),
-                          IconButton(
-                              icon: FaIcon(FontAwesomeIcons.solidComment,
-                                  color: Colors.white),
-                              onPressed: () {
-                                print("Pressed");
-                              }),
-                          IconButton(
-                              icon: FaIcon(FontAwesomeIcons.trash,
-                                  color: Colors.white),
-                              onPressed: () {
-                                print("Pressed");
-                              }),
-                        ]),
-                      ],
-                    ),
-                  ]),
-            ],
+                SizedBox(height: 5.0),
+                Row(children: <Widget>[
+                  Chip(
+                    padding: EdgeInsets.all(0),
+                    backgroundColor: Colors.white,
+                    label: Text('React',
+                        style: GoogleFonts.lato(color: primaryColor, fontSize: 12.0)),
+                  ),
+                  SizedBox(width: 10.0),
+                  Chip(
+                    padding: EdgeInsets.all(0),
+                    backgroundColor: Colors.white,
+                    label: Text('Javascript',
+                        style: GoogleFonts.lato(color: primaryColor, fontSize: 12.0)),
+                  ),
+                ]),
+                SizedBox(height: 10.0),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          SvgPicture.asset(logo,
+                              height: 40, width: 40, fit: BoxFit.scaleDown),
+                          SizedBox(width: 8.0),
+                          Text(
+                            'Rachel',
+                            style: GoogleFonts.lato(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 15.0),
+                      Row(
+                        children: <Widget>[
+                          Row(children: <Widget>[
+                            IconButton(
+                                icon: FaIcon(
+                                    FontAwesomeIcons.solidCheckCircle,
+                                    color: Colors.white),
+                                onPressed: () {
+                                  print("Pressed");
+                                }),
+                            IconButton(
+                                icon: FaIcon(FontAwesomeIcons.solidComment,
+                                    color: Colors.white),
+                                onPressed: () {
+                                  print("Pressed");
+                                }),
+                            IconButton(
+                                icon: FaIcon(FontAwesomeIcons.trash,
+                                    color: Colors.white),
+                                onPressed: () {
+                                  print("Pressed");
+                                }),
+                          ]),
+                        ],
+                      ),
+                    ]),
+              ],
+            ),
           ),
         ),
       ),
