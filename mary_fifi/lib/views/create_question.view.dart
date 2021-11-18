811a038f8494e8d0f2b0698a6d08fed782dc0b25
gabiliz <gabrielalizmoreira@gmail.com>
2021-11-17 22:02:49 -0300

    criar pergunta atualizada

--- mary_fifi/lib/views/create_question.view.dart
+++ mary_fifi/lib/views/create_question.view.dart
@@ -11,108 +11,131 @@ class CreateQuestion extends StatelessWidget {
         home: Scaffold(
       backgroundColor: primaryColor,
       body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Criar pergunta',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(
-              fontSize: 25,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          TextField(
-            cursorColor: Colors.white,
-            style: TextStyle(color: Colors.white),
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(vertical: 60.0),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xffA04FC6), width: 1),
-                  borderRadius: BorderRadius.circular(16)),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0xffA04FC6), width: 1),
-                  borderRadius: BorderRadius.circular(16)),
-            ),
-          ),
-          Text(
-            'Perguntando como',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.poppins(
-              fontSize: 16,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              SvgPicture.asset(logo,
-                  height: 40, width: 40, fit: BoxFit.scaleDown),
-              SizedBox(width: 8.0),
-              Text(
-                'João Gomes',
-                style: GoogleFonts.lato(color: Colors.white),
-              ),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                      builder: (context) => new Room(),
+        child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text(
+                    'Criar pergunta',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                },
-                style: ButtonStyle(
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                        EdgeInsets.all(15)),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(100.0)))),
-                child: Text(
-                  "Voltar",
-                  style: GoogleFonts.lato(
-                    fontSize: 15.0,
-                    color: Color(0xff2A2235),
                   ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                      builder: (context) => new Room(),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextField(
+                    cursorColor: Colors.white,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(vertical: 60.0),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffA04FC6), width: 1),
+                          borderRadius: BorderRadius.circular(16)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffA04FC6), width: 1),
+                          borderRadius: BorderRadius.circular(16)),
                     ),
-                  );
-                },
-                style: ButtonStyle(
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                        EdgeInsets.all(15)),
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(100.0)))),
-                child: Text(
-                  "Criar pergunta",
-                  style: GoogleFonts.lato(
-                    fontSize: 15.0,
-                    color: Color(0xff2A2235),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
-      )),
+                Container(
+                  margin: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    'Perguntando como',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 30.0),
+                  child: Row(
+                    children: <Widget>[
+                      SvgPicture.asset(logo,
+                          height: 40, width: 40, fit: BoxFit.scaleDown),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'João Gomes',
+                        style: GoogleFonts.lato(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                            builder: (context) => new Room(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              EdgeInsets.fromLTRB(25, 15, 25, 15)),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(100.0)))),
+                      child: Text(
+                        "Voltar",
+                        style: GoogleFonts.lato(
+                          fontSize: 15.0,
+                          color: Color(0xff2A2235),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 25.0),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                            builder: (context) => new Room(),
+                          ),
+                        );
+                      },
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              EdgeInsets.all(15)),
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(100.0)))),
+                      child: Text(
+                        "Criar pergunta",
+                        style: GoogleFonts.lato(
+                          fontSize: 15.0,
+                          color: Color(0xff2A2235),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ),
     ));
   }
 }
